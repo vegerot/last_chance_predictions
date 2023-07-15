41ab8999f01731cad1c125295422f78df341f5f7
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-15 15:51:00 -0700

    refactor: move AppState schema into its own file

---
+++ extension/AppState.d.ts
@@ -0,0 +1,19 @@
+// predictionSettings: {
+//   status: 'active' | 'locked' | 'none',
+//   // non-null if .status !== 'none':
+//   outcomes: {color: string, iconURI: string, name: string}[] | null,
+//   // non-null if .status !== 'none':
+//   title: string | null,
+//   // non-null if .status === 'active'
+//   deadlineTimeMS: number | null,
+// }
+// userSettings: {
+//   predictionRatios: number[],
+//   pointLimit: number | null,
+//   secondsBeforeDeadline: number | null,
+//   enabled: boolean,
+// }
+// submission: null | {
+//   points: number,
+//   // TODO(strager): outcomeIndex
+// }
