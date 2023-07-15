f1c9aeb7838fd85906415335a3487d9efce86712
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-15 00:44:19 -0700

    feat(ui): support 'locked' prediction status, among other tweaks
    Some state isn't synced to the UI.

--- extension/scripts/final_fate_popup_script.js
+++ extension/scripts/final_fate_popup_script.js
@@ -16,11 +16,36 @@ function initUI() {
     }
 }
 
+// status: 'active' | 'locked' | 'none'
 // outcomes: {color: string, iconURI: string, name: string}[]
-function predictionStarted({title, deadlineTimeMS, outcomes}) {
+// predictedPoints: number | null
+// userPredictionRatios: number[]
+// userPointLimit: number | null
+// userSecondsBeforeDeadline: number | null
+// TODO(strager): predictedOutcomeIndex
+function predictionStateUpdated({
+    status,
+    title,
+    deadlineTimeMS,
+    outcomes,
+    predictedPoints,
+    userPredictionRatios,
+    userPointLimit,
+    userEnabled,
+    userSecondsBeforeDeadline,
+}) {
+    console.assert(userPredictionRatios.reduce((a, b) => a+b, 0) === 100, `prediction ratios should add up to 100: ${userPredictionRatios}`);
+    console.assert(userPredictionRatios.length === outcomes.length, 'number of outcomes must equal number of prediction ratios');
+
     startPredictionTimer(deadlineTimeMS);
 
-    let rootElement = document.querySelector('#active-prediction');
+    let rootElement = document.querySelector('#prediction');
+    rootElement.classList.toggle('status-active', status === 'active');
+    rootElement.classList.toggle('status-locked', status === 'locked');
+    rootElement.classList.toggle('status-none', status === 'none');
+    rootElement.classList.toggle('predicted-points', predictedPoints !== null);
+    rootElement.classList.toggle('no-predicted-points', predictedPoints === null);
+
     rootElement.querySelector('.title').textContent = title;
     if (outcomes.length === 2) {
         let dualOutcomesElement = rootElement.querySelector('.dual-prediction');
@@ -34,6 +59,24 @@ function predictionStarted({title, deadlineTimeMS, outcomes}) {
         dualOutcomesElement.style.setProperty('--outcome-b-color', outcomes[1].color);
         initOutcomeUI(outcomeAElement, outcomes[0]);
         initOutcomeUI(outcomeBElement, outcomes[1]);
+
+        let predictionElement = dualOutcomesElement.querySelector('[name="prediction"]');
+        setValueIfDifferent(predictionElement, userPredictionRatios[0]);
+        updatePredictionSliderCSS(predictionElement);
+    }
+
+    setValueIfDifferent(rootElement.querySelector('[name="seconds-before-deadline"]'), userSecondsBeforeDeadline);
+    setValueIfDifferent(rootElement.querySelector('[name="point-limit"]'), userPointLimit);
+    rootElement.querySelector('[name="enable"]').value = userEnabled;
+
+    rootElement.querySelector('.points-predicted').textContent = `${predictedPoints}`;
+    rootElement.querySelector('.point-limit').textContent = `${userPointLimit}`;
+}
+
+// setValueIfDifferent prevents moving the user's text cursor unnecessarily.
+function setValueIfDifferent(element, newValue) {
+    if (String(element.value) !== String(newValue)) {
+        element.value = newValue;
     }
 }
 
@@ -59,14 +102,27 @@ function updatePredictionTimer() {
 }
 
 function test() {
-    predictionStarted({
+    let deadline = Date.now() + 2 * 60 * 1000;
+    let state = {
+        status: 'active',
         title: 'Collect supers by 15:00?',
-        deadlineTimeMS: Date.now() + 2 * 60 * 1000,
+        deadlineTimeMS: deadline,
         outcomes: [
             {color: 'pink', iconURI: '', name: 'YES'},
             {color: 'blue', iconURI: '', name: 'no'},
         ],
-    });
+        predictedPoints: null,
+        userPredictionRatios: [30, 70],
+        userPointLimit: 6969,
+        userEnabled: false,
+        userSecondsBeforeDeadline: 3,
+    };
+
+    predictionStateUpdated({...state});
+
+    setTimeout(() => {
+        predictionStateUpdated({...state, status: 'locked', predictedPoints: 69});
+    }, 2000);
 }
 
 initUI();
