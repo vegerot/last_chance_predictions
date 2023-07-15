8c8fa21fcbfce9363f7d3b483289c3fc65d531ad
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-14 23:57:26 -0700

    feat(ui): add basic sketch for the UI

--- extension/scripts/final_fate_popup_script.js
+++ extension/scripts/final_fate_popup_script.js
@@ -1,1 +1,14 @@
-console.log("loaded final_fate_popup_script.js");
+document.body.addEventListener('input', (event) => {
+    let target = event.target;
+    if (target.classList.contains('prediction-slider')) {
+        updatePredictionSliderCSS(target);
+    }
+});
+
+function updatePredictionSliderCSS(element) {
+    element.style.setProperty('--value', `${element.value}%`)
+}
+
+for (let predictionSliderElement of document.querySelectorAll('.prediction-slider')) {
+    updatePredictionSliderCSS(predictionSliderElement);
+}
