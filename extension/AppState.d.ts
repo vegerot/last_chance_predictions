7eeecf152b73f6fc475e4741e8cc4d44b7a6209d
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-07-15 15:52:40 -0700

    refactor: make real TypeScript code

--- extension/AppState.d.ts
+++ extension/AppState.d.ts
@@ -1,19 +1,24 @@
-// predictionSettings: {
-//   status: 'active' | 'locked' | 'none',
-//   // non-null if .status !== 'none':
-//   outcomes: {color: string, iconURI: string, name: string}[] | null,
-//   // non-null if .status !== 'none':
-//   title: string | null,
-//   // non-null if .status === 'active'
-//   deadlineTimeMS: number | null,
-// }
-// userSettings: {
-//   predictionRatios: number[],
-//   pointLimit: number | null,
-//   secondsBeforeDeadline: number | null,
-//   enabled: boolean,
-// }
-// submission: null | {
-//   points: number,
-//   // TODO(strager): outcomeIndex
-// }
+interface AppState extends ChannelState {
+}
+
+interface ChannelState {
+  predictionSettings: {
+    status: 'active' | 'locked' | 'none',
+    // non-null if .status !== 'none':
+    outcomes: {color: string, iconURI: string, name: string}[] | null,
+    // non-null if .status !== 'none':
+    title: string | null,
+    // non-null if .status === 'active'
+    deadlineTimeMS: number | null,
+  };
+  userSettings: {
+    predictionRatios: number[],
+    pointLimit: number | null,
+    secondsBeforeDeadline: number | null,
+    enabled: boolean,
+  };
+  submission: null | {
+    points: number,
+    // TODO(strager): outcomeIndex
+  };
+}
